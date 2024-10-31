04433f07fd8b45acfea51a83377db8a892ab5643
thihonggamtran92 <thihonggamtran92@outlook.com>
2024-10-31 07:09:27 -0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav.js";
+import Promo from "./components/Promo.js";
+import Intro1 from "./components/Intro1.js";
+import Intro2 from "./components/Intro2.js";
+import Intro3 from "./components/Intro3.js";
+import Footer from "./components/Footer.js";
+
+import Heading from "./Heading.js";
+
+// function Heading() {
+//   let title = "This is an h1 heading";
+//   return <h1>{title}</h1>;
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello React.</h1> */}
+      <Heading />
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Nav />
+//       <Promo />
+//       <Intro1 />
+//       <Intro2 />
+//       <Intro3 />
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Heading.js
@@ -0,0 +1,5 @@
+function Heading() {
+  let title = "This is an h1 heading";
+  return <h1>{title}</h1>;
+}
+export default Heading;
